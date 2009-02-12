79e672f16ac1a617ca38d5d4c59f60285268d726
Joshua Nussbaum <joshnuss@gmail.com>
2009-02-11 21:47:44 -0500

    copy config/database.yml as database_exmaple.yml

--- base.rb
+++ base.rb
@@ -1,17 +1,16 @@
 run "echo TODO > README"
 
 plugin 'jrails', :svn => "http://ennerchi.googlecode.com/svn/trunk/plugins/jrails"
+plugin 'rspec', :git => "git://github.com/dchelimsky/rspec.git"
+plugin 'rspec-rails', :git => "git://github.com/dchelimsky/rspec-rails.git"
+generate :rspec
 
-if yes?("Do you want to use RSPEC?")
-  plugin 'rspec', :git => "git://github.com/dchelimsky/rspec.git"
-  plugin 'rspec-rails', :git => "git://github.com/dchelimsky/rspec-rails.git"
-  generate :rspec
-end
-
+run "cp config/database.yml config/database_example.yml"
 run "rm public/index.html"
 
 git :init
 file ".gitignore", <<-END
+config/database.yml
 log/*.log
 log/*.pid
 tmp/**/*
@@ -22,8 +21,7 @@ doc/rspec*
 db/*.sqlite3
 public/upload/*
 public/download/*
-public/system/photos/*
-db/sphinx/*
+public/system/*
 *.swp
 END
 git :add => '.', :commit => "-m 'initial commit'"
